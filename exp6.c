aaca25cba5cfbd062e919212355cb7a0b4ed20be
getchges <149615696+getchges@users.noreply.github.com>
2024-04-01 13:46:55 +0000

    done

--- exp6.c
+++ exp6.c
@@ -3,9 +3,9 @@
 
 int main() 
 { 
-    int i, limit, total = 0, x, counter = 0, time_quantum; 
-    int wait_time = 0, turnaround_time = 0, arrival_time[10], burst_time[10], temp[10]; 
-    float average_wait_time, average_turnaround_time; 
+    int i, limit, total = 0, x, counter = 0, tq; 
+    int wt = 0, tat = 0, at[10], bt[10], temp[10]; 
+    float avgwt, avgtat; 
     
     printf("Enter Total Number of Processes: "); 
     scanf("%d", &limit); 
@@ -15,19 +15,19 @@ int main()
     { 
         printf("Enter Details of Process[%d]\n", i + 1); 
         printf("Arrival Time: "); 
-        scanf("%d", &arrival_time[i]); 
+        scanf("%d", &at[i]); 
         printf("Burst Time: "); 
-        scanf("%d", &burst_time[i]); 
-        temp[i] = burst_time[i]; 
+        scanf("%d", &bt[i]); 
+        temp[i] = bt[i]; 
     } 
     
     printf("Enter Time Quantum: "); 
-    scanf("%d", &time_quantum); 
+    scanf("%d", &tq); 
     printf("\nProcess ID\tBurst Time\t Turnaround Time\t Waiting Time\n"); 
     
     for(total = 0, i = 0; x != 0;) 
     { 
-        if(temp[i] <= time_quantum && temp[i] > 0) 
+        if(temp[i] <= tq && temp[i] > 0) 
         { 
             total = total + temp[i]; 
             temp[i] = 0; 
@@ -35,22 +35,22 @@ int main()
         } 
         else if(temp[i] > 0) 
         { 
-            temp[i] = temp[i] - time_quantum; 
-            total = total + time_quantum; 
+            temp[i] = temp[i] - tq; 
+            total = total + tq; 
         } 
         if(temp[i] == 0 && counter == 1) 
         { 
             x--; 
-            printf("Process[%d]\t%d\t\t%d\t\t%d\n", i + 1, burst_time[i], total - arrival_time[i], total - arrival_time[i] - burst_time[i]); 
-            wait_time = wait_time + total - arrival_time[i] - burst_time[i]; 
-            turnaround_time = turnaround_time + total - arrival_time[i]; 
+            printf("Process[%d]\t%d\t\t%d\t\t%d\n", i + 1, bt[i], total - at[i], total - at[i] - bt[i]); 
+            wt = wt + total - at[i] - bt[i]; 
+            tat = tat + total - at[i]; 
             counter = 0; 
         } 
         if(i == limit - 1)
         {
             i = 0; 
         } 
-        else if(arrival_time[i + 1] <= total)
+        else if(at[i + 1] <= total)
         { 
             i++; 
         } 
@@ -60,11 +60,11 @@ int main()
         } 
     } 
     
-    average_wait_time = wait_time * 1.0 / limit; 
-    average_turnaround_time = turnaround_time * 1.0 / limit; 
+    avgwt = wt * 1.0 / limit; 
+    avgtat = tat * 1.0 / limit; 
     
-    printf("\nAverage Waiting Time: %f", average_wait_time); 
-    printf("\nAvg Turnaround Time: %f\n", average_turnaround_time); 
+    printf("\nAverage Waiting Time: %f", avgwt); 
+    printf("\nAvg Turnaround Time: %f\n", avgtat); 
     
     return 0; 
 } 
